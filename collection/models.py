09f6896d5e8dbe583a91ce3922dce37f4cb944dc
SimplyFaisal <d.f.animashaun@gmail.com>
2015-03-06 15:11:34 -0500

    moved to flask.sqlalchemy

--- collection/models.py
+++ collection/models.py
@@ -1,15 +1,49 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-DATABASE_URI = None
+DATABASE_URI = 'mysql://root:password@localhost/proto_thrum' 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 db = SQLAlchemy(app)
 
-class Model(db.model):
+class Post(db.Model):
+    
+    id = db.Column(db.String(10), primary_key=True)
+    title = db.Column(db.String(10000), nullable=False)
+    text = db.Column(db.String(10000), nullable=True)
+    url = db.Column(db.String(1000), nullable=True)
+    ups = db.Column(db.Integer, nullable=False)
+    downs = db.Column(db.Integer, nullable=False)
 
-    def __init__(self):
-        pass
+    def __init__(self, id, title, text, url, ups, downs):
+        self.id = id
+        self.title  = title
+        self.text = text
+        self.url = url
+        self.ups = ups
+        self.downs = downs
 
     def __repr__(self):
-        pass
+        return '<Post {}>'.format(self.title)
+
+
+class Comment(db.Model):
+
+    id = db.Column(db.String(10), primary_key=True)
+    body = db.Column(db.String(10000), nullable=True)
+    ups = db.Column(db.Integer, nullable=False)
+    downs = db.Column(db.Integer, nullable=False)
+    post_id = db.Column(db.String(10), db.ForeignKey('post.id'))
+    post = db.relationship(Post)
+    
+    def __init__(self, id, body, ups, downs, post_id, post):
+        self.id = id
+        self.body = body
+        self.ups = ups
+        self.downs = downs
+        self.post_id = post_id
+        self.post = post
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.body) 
+
--- collection/rcrawler_update.py
+++ collection/rcrawler_update.py
@@ -1,26 +1,33 @@
 import praw
 import time
 from pprint import pprint
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from rcrawler_declarative import Post, Comment, Base
+from models import db, Post, Comment
 
-engine = create_engine("mysql+mysqldb://root:rtrad@localhost/yaks")
-Base.metadata.bind = engine
+def main():
+    r = praw.Reddit('PRAW Gatech subreddit monitor')
+    r.login('elc-gt', 'password')
+    subreddit = r.get_subreddit('gatech')
+    for submission in subreddit.get_new(limit=30):
+        print "Getting submission:  {}".format(submission.title)
+        new_post = Post(id=submission.id, 
+                        title=submission.title, 
+                        text=submission.selftext.encode('utf-8'),
+                        url=submission.url, 
+                        ups=submission.ups, 
+                        downs=submission.downs)
+        db.session.merge(new_post)
+        db.session.commit()
+        comments = praw.helpers.flatten_tree(submission.comments)
+        for comment in comments:
+            print "Getting Comment for: {}".format(submission.title)
+            new_comment = Comment(id=comment.id, 
+                                  body=comment.body.encode('utf-8'), 
+                                  ups=comment.ups, 
+                                  downs=comment.downs,
+                                  post_id=submission.id ,
+                                  post=new_post)
+            db.session.merge(new_comment)
+            db.session.commit()
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
-r = praw.Reddit('PRAW Gatech subreddit monitor')
-r.login()
-
-subreddit = r.get_subreddit('gatech')
-for submission in subreddit.get_new(limit=30):
-    new_post = Post(id=submission.id, title=submission.title, text=submission.selftext.encode('utf-8'), url=submission.url, ups=submission.ups, downs=submission.downs)
-    session.merge(new_post)
-    session.commit()
-    comments = praw.helpers.flatten_tree(submission.comments)
-    for comment in comments:
-        new_comment = Comment(id=comment.id, body=comment.body.encode('utf-8'), ups=comment.ups, downs=comment.downs, post=new_post)
-        session.merge(new_comment)
-        session.commit()
+if __name__ == '__main__':
+    main()
