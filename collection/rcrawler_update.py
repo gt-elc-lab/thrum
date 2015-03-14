aa1265294384a347220715d0c1d99eef920ab447
SimplyFaisal <d.f.animashaun@gmail.com>
2015-03-14 19:50:47 -0400

    crawler does not crash

--- collection/rcrawler_update.py
+++ collection/rcrawler_update.py
@@ -1,36 +1,46 @@
 import praw
-import time
 from models import db, Post, Comment
 from config import SUBREDDITS, USERNAME, PASSWORD
+from datetime import datetime
 
 
 def main():
     reddit = praw.Reddit('PRAW Gatech subreddit monitor')
-    print "logging in"
+    print "Logging in"
     reddit.login(USERNAME, PASSWORD)
-    print "logged in. about to scrape"
+    print "Logged in. Starting to scrape"
     for school, subreddit in SUBREDDITS.iteritems():
-        print "scraping {} : {}".format(school, subreddit)
+        print "Scraping {} : r/{}".format(school, subreddit)
+        start = datetime.now()
         posts = reddit.get_subreddit(subreddit)
-        crawl_subreddit(posts.get_new(limit=30), school, subreddit)
+        num_posts, num_comments = crawl_subreddit(posts.get_new(limit=30), school, subreddit)
+        duration = datetime.now() - start
+        output = "Finished scraping {} : r/{} | {} posts {} comments | {} seconds"
+        print output.format(school, subreddit, num_posts, num_comments, 
+                                                        duration.seconds)
+    print "Done"
     
 
 def crawl_subreddit(posts, school, subreddit):
-    for submission in posts:
-        new_post = Post(id=submission.id, 
-                        title=submission.title.encode('utf-8'),
-                        text=submission.selftext.encode('utf-8'),
-                        url=submission.url, 
-                        ups=submission.ups, 
-                        downs=submission.downs,
-                        subreddit=subreddit,
-                        college=school,
-                        create_utc=submission.created_utc)
-        db.session.merge(new_post)
-        db.session.commit()
-        comments = praw.helpers.flatten_tree(submission.comments)
-        for comment in comments:
-            if isinstance(comment, praw.objects.Comment):
+    num_posts = 0
+    num_comments = 0
+    try: 
+        for submission in posts:
+            new_post = Post(id=submission.id, 
+                            title=submission.title.encode('utf-8'),
+                            text=submission.selftext.encode('utf-8'),
+                            url=submission.url, 
+                            ups=submission.ups, 
+                            downs=submission.downs,
+                            subreddit=subreddit,
+                            college=school,
+                            create_utc=submission.created_utc)
+            db.session.merge(new_post)
+            db.session.commit()
+            num_posts += 1
+            submission.replace_more_comments(limit=None, threshold=0)
+            comments = praw.helpers.flatten_tree(submission.comments)
+            for comment in comments:
                 new_comment = Comment(id=comment.id, 
                                       body=comment.body.encode('utf-8'), 
                                       ups=comment.ups, 
@@ -40,9 +50,12 @@ def crawl_subreddit(posts, school, subreddit):
                                       create_utc=submission.created_utc)
                 db.session.merge(new_comment)
                 db.session.commit()
-            else:
-                continue
-
+                num_comments += 1
+    except AssertionError as e:
+        pass
+       
+    return (num_posts, num_comments)
+            
 if __name__ == '__main__':
     main()
     
