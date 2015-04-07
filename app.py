9a4b61bf48b309cbe3722e583c92e3e1474da50e
SimplyFaisal <d.f.animashaun@gmail.com>
2015-04-07 18:59:53 -0400

    word tree stuff

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, json, render_template, request, jsonify
+from nltk.tokenize import sent_tokenize
 from datetime import datetime, timedelta
 from collection.models import Post, Comment, db
 from collection.nltk_20 import WordFrequency
@@ -24,12 +25,16 @@ def send_data():
     result = []
     for school in colleges:
         corpus = query_word(school, term)
-        buckets = bucketize(corpus, school)
+        buckets = bucketize(corpus, school, term)
         if buckets:
             result += buckets
     return render_template('line_graph.html', colleges=dropdown,
                                               results=jsonify(data=result))
 
+@app.route('/<college>/<word>/<time_stamp>')
+def word_tree(college, word, time_stamp):
+    corpus = query_interval(college, word, time_stamp)
+    return jsonify(data=word_tree_data(corpus))
 
 def query_word(college, word):
     post_results = Post.query.whoosh_search(word).filter(Post.college==college)
@@ -37,9 +42,42 @@ def query_word(college, word):
     corpus = post_results.all() + comments_results.all()
     return corpus
 
-def bucketize(corpus, school):
+def bucketize(corpus, school, term):
     serializer = TimeSerializer()
-    return serializer.weekly_buckets(corpus, school)
+    return serializer.weekly_buckets(corpus, school, term)
+
+def word_tree_data(posts):
+    corpus = ""
+    for post in posts:
+        if isinstance(post, Post):
+            corpus += post.title
+            corpus += post.text
+        if isinstance(post, Comment):
+            corpus += post.body
+    return sent_tokenize(corpus)
+
+def query_interval(college, word, time_stamp):
+    date = datetime.fromtimestamp(float(time_stamp) / 1000)
+    week_before = date - timedelta(weeks=1)
+    week_after = date + timedelta(weeks=1)
+    posts = Post.query.whoosh_search(word).filter(Post.college==college, 
+                                                Post.created.between(week_before, week_after))
+    comments = Comment.query.whoosh_search(word).filter(Comment.college==college, 
+                                                Comment.created.between(week_before, week_after))
+    result = posts.all()  + comments.all()
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # posts = Post.query.filter(Post.college=='Georgia Tech').all()[:300]
+    # corpus = [post.text for post in posts]
+    # text = "".join([p for p in corpus])
+    # tf = TFIDF(corpus)
+    # a=[]
+    # for c in corpus:
+    #     a+= tf.batch_tfidf(c)
+    # a = sorted(a, key=lambda x:x['value'])
+    # for word in a:
+    #     print word
+
+
